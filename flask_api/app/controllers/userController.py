140733f7071a9d8db62dc9503402dca0e00e4750
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2019-12-02 21:41:47 +0300

    User Controller

--- flask_api/app/controllers/userController.py
+++ flask_api/app/controllers/userController.py
@@ -9,3 +9,24 @@ from app.helpers.isAuth import isAuth
 
 # Blue print
 bluePrint = Blueprint('users', __name__, url_prefix='/users')
+
+@bluePrint.route("/getUser", methods=["GET"])
+@isAuth(request)
+def getQuestion(user):
+
+    # ID as parameter
+    userId = request.args.get('id')
+
+    result = None
+    try:
+        result = User({"_id": userId}).data()
+    except Exception as e:
+        print("NO SUCH USER ID")
+
+    status = result is not None
+
+    # Return the response
+    return jsonify({
+        'success': status,
+        "user": result
+    }), 200
