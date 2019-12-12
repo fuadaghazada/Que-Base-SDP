8225c136e11f86017e2af6ca5a026c9c33ebfb62
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2019-12-12 20:52:06 +0300

    User Overview Styling

--- react_client/src/components/users/UserOverview.js
+++ react_client/src/components/users/UserOverview.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 480,
+    backgroundColor: theme.palette.background.paper,
+  },
+  dividerFullWidth: {
+    margin: `5px 0 0 ${theme.spacing(2)}px`,
+  }
+}));
 
 const UserOverview = (props) => {
-
-    const { firstname, lastname } = props.userData;
+    const classes = useStyles();
+    const { firstname, lastname, username } = props.userData;
 
     return (
-        <div>
-            <h1>{firstname}</h1>
-            <h1>{lastname}</h1>
-        </div>
+        <List className={classes.root}>
+            <ListItem>
+                <ListItemText primary="Username" secondary={username} />
+            </ListItem>
+            <Divider component="li" />
+            <li>
+                <Typography
+                    className={classes.dividerFullWidth}
+                    color="textSecondary"
+                    display="block"
+                    variant="caption"
+                >
+                </Typography>
+            </li>
+            <ListItem>
+                <ListItemText primary="Full Name" secondary={firstname + " " + lastname} />
+            </ListItem>
+            <Divider component="li" />
+        </List>
     )
 };
 
